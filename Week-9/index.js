a2a8d02d40cb061ff795d367c06a4cefcbe2ade9
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-03-21 19:29:54 +0530

    Week-9: Signup endpoint added

--- Week-9/index.js
+++ Week-9/index.js
@@ -6,12 +6,37 @@ const port = 3000
 app.use(express.json())
 
 const notes = [];
+const users = [];
+
+// authentication endpoints
+// signup endpoint
+app.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const userExists = users.find(user => user.username === username);
+  if(username){
+    return res.status(403).json({
+      message : "user already exists"
+    })
+  }
+
+  users.push({
+    username: username,
+    password: password
+  })
+
+  res.json({
+    message: "user created successfully"
+  })
+})
 
 //frontend endpoint
 app.get('/', (req, res) => {
   res.sendFile('D:\\Bootcamp-0.1-MERN-Stack\\Week-9\\frontend\\index.html')
 })
 
+// backend endpoint
 app.post('/notes', (req,res) => {
   const note = req.body.note;
 
@@ -22,11 +47,13 @@ app.post('/notes', (req,res) => {
   })
 })
 
+// backend endpoint
 app.get('/notes', (req, res) => {
   res.json({
     notes
   })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
