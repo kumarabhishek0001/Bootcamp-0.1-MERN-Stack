a9d4cc5edf5af9607a320144ff5b0f615be17592
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-03-22 00:56:03 +0530

    Week-9: Notes app auth added

--- Week-9/index.js
+++ Week-9/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
@@ -6,7 +7,10 @@ const port = 3000
 app.use(express.json())
 
 const notes = [];
-const users = [];
+const users = [{
+  username: "abhishek",
+  password: "123123"
+}];
 
 // authentication endpoints
 // signup endpoint
@@ -15,7 +19,8 @@ app.post('/signup', (req, res) => {
   const password = req.body.password;
 
   const userExists = users.find(user => user.username === username);
-  if(username){
+
+  if(userExists){
     return res.status(403).json({
       message : "user already exists"
     })
@@ -31,6 +36,32 @@ app.post('/signup', (req, res) => {
   })
 })
 
+app.post('/signin', (req, res)=> {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const userExists = users.find(user => {
+    return user.username === username && user.password === password;
+  })
+
+  if(!userExists){
+    return res.status(403).json({
+      message: "Invalid credentials"
+    })
+  }
+
+
+  // sending a jwt token
+  const token = jwt.sign({
+    username: username
+  }, "harkirat123");
+
+
+  res.json({
+    token: token
+  })
+})
+
 //frontend endpoint
 app.get('/', (req, res) => {
   res.sendFile('D:\\Bootcamp-0.1-MERN-Stack\\Week-9\\frontend\\index.html')
@@ -38,9 +69,28 @@ app.get('/', (req, res) => {
 
 // backend endpoint
 app.post('/notes', (req,res) => {
+
+  const token = req.headers.token;
+  if(!token){
+    res.status(403).send({
+      message: "Unauthorized user"
+    })
+
+    return; 
+  }
+
+  const decoded = jwt.verify(token, "harkirat123");
+  const username = decoded.username;
+
+  if(!username){
+    res.status(403).json({
+      message: "malformed token"
+    })
+  }
+
   const note = req.body.note;
 
-  notes.push(note);
+  notes.push({note, username});
 
   res.json({
     message: "Note added successfully!!"
@@ -49,8 +99,29 @@ app.post('/notes', (req,res) => {
 
 // backend endpoint
 app.get('/notes', (req, res) => {
+
+  const token = req.headers.token;
+  if(!token){
+    res.status(403).send({
+      message: "Unauthorized user"
+    })
+
+    return; 
+  }
+
+  const decoded = jwt.verify(token, "harkirat123");
+  const username = decoded.username;
+
+  if(!username){
+    res.status(403).json({
+      message: "malformed token"
+    })
+  }
+
+  const usernote = notes.filter(note => note.username === username);
+
   res.json({
-    notes
+    notes: usernote
   })
 })
 
