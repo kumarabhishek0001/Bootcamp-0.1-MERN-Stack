694079573a03f52411dd7d105f01ab1622a9746c
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-03-27 23:52:17 +0530

    WEEK-11 (FEATURE): sign up added

---
+++ Week-11/index.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const PORT = 3002;
+
+
+let CURRENT_USER_ID = 1;
+let CURRENT_TODO_ID = 1;
+
+let USERS = [];
+let TODOS = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+// CREATE endpoints
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExists = USERS.find(user => user.username === username);
+
+    if(userExists){
+        return res.status(422).json({
+            message: "User already exists"
+        })
+    }
+
+    const userData = {
+        userId: CURRENT_USER_ID++,
+        username,
+        password
+    }
+
+    USERS.push(userData);
+
+    res.status(201).send(USERS);
+})
+
+
+app.post('/signin', (req, res) => {
+
+    
+})
+// app.post('todo')
+
+// READ endpoints
+// app.get('/todos')
+
+// UPDATE endpoints
+// app.put('/todo/:todoId')
+
+// DELETE endpoints
+// app.delete('/todo/:todoId')
+
+app.listen(PORT, () => {
+    console.log("App listening to "+chalk.green('http://localhost:'+PORT))
+})
